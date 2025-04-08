ec7804356e1d5763bef35e3796c743cceaf9ec77
Rafael <faeel-xd@hotmail.com>
2025-04-08 14:02:40 -0300

    Segundo commit

--- personal_trainer/Login.java
+++ personal_trainer/Login.java
@@ -6,6 +6,9 @@ public class Login {
         Student rafael = new Student("rafael.scarvalho08@gmail.com", "31145302Rafaau");
         System.out.println(rafael.validaEntrada(1));
 
+        Student higor = new Student("higor.nunes@gmail.com", "higorgamer123");
+        System.out.println(higor.validaEntrada(1));
+
         //Professor camila = new Professor("camila.nunes@outlook.com", "teste12");
         //System.out.println(camila.validaEntrada(2));
     }
